c2d3431c7b6a6856121396a526768bea9d2878d8
Baptiste JAOUEN <Baptiste.JAOUEN@afpa-montpellier.fr>
2024-01-16 15:51:22 +0100

    Premiere modif Baptiste

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php 
 
-
+echo "Lorem dolor sit amet consectetur adipisicing elit. Cumque porro omnis error eum repellendus libero earum. Molestias, consequuntur laborum recusandae est rerum aliquid? Ipsam odit eius, maiores sint magni officiis."; 
