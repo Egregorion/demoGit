1bd58c107fdc9158ebd41a46aae1cc68ffe6f11c
Egregorion <gregory.fourmaux@gmail.com>
2024-01-17 16:43:23 +0100

    ajout de la boucle for

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
 <?php 
 
 echo "Lorem dolor sit amet consectetur adipisicing elit. Cumque porro omnis error eum repellendus libero earum. Molestias, consequuntur laborum recusandae est rerum aliquid? Ipsam odit eius, maiores sint magni officiis."; 
+echo "<br />";
+echo "<br />";
+$user_input = 5;
+for($i=1; $i<=10; $i++)
+{
+    echo $user_input."X".$i."=".$user_input*$i."<br />";
+}
